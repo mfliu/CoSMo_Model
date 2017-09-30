8e3a4b0c3e141ae557a27b1090924410a0f24655
Monica Liu <liu.monica.f@gmail.com>
2017-09-30 17:25:55 -0400

    Running on cosmo model, weird trajectories, doesn't reach target

--- hyperparams.py
+++ hyperparams.py
@@ -1,12 +1,10 @@
-# To get started, copy over hyperparams from another experiment.
-# Visit rll.berkeley.edu/gps/hyperparams.html for documentation.
-
 from __future__ import division
 
 from datetime import datetime
 import os.path
 import numpy as np
 
+from gps.algorithm.algorithm_traj_opt_pilqr import AlgorithmTrajOptPILQR
 from gps import __file__ as gps_filepath
 from gps.agent.mjc.agent_mjc import AgentMuJoCo
 from gps.algorithm.algorithm_badmm import AlgorithmBADMM
@@ -23,6 +21,7 @@ from gps.algorithm.policy.policy_prior_gmm import PolicyPriorGMM
 from gps.algorithm.policy_opt.tf_model_example import tf_network
 from gps.proto.gps_pb2 import JOINT_ANGLES, JOINT_VELOCITIES, END_EFFECTOR_POINTS, END_EFFECTOR_POINT_VELOCITIES, ACTION
 from gps.gui.config import generate_experiment_info
+from gps.algorithm.cost.cost_utils import RAMP_CONSTANT
 
 ALGORITHM_NN_LIBRARY = "tf"
 
@@ -34,9 +33,7 @@ SENSOR_DIMS = {
         ACTION:7,
         }
 
-PR2_GAINS = np.array([3.09, 1.08, 0.393, 0.674, 0.111, 0.152, 0.098])
-# Monica added to test
-#PR2_GAINS = np.ones(6) * 0.05
+GAINS = np.array([500, 500, 500, 1, 0.01, 0.0025, 0.0025]) 
 
 BASE_DIR = '/'.join(str.split(gps_filepath, '/')[:-2])
 EXP_DIR = BASE_DIR + '/../experiments/CoSMo/'
@@ -49,7 +46,7 @@ common = {
     'data_files_dir': EXP_DIR + 'data_files/',
     'target_filename': EXP_DIR + 'target.npz',
     'log_filename': EXP_DIR + 'log.txt',
-    'conditions': 4,
+    'conditions': 1,
 }
 
 if not os.path.exists(common['data_files_dir']):
@@ -58,42 +55,41 @@ if not os.path.exists(common['data_files_dir']):
 agent = {
     'type': AgentMuJoCo,
     'filename': './mjc_models/cosmo_model.xml',
-    'x0': np.concatenate((np.array([0.1, 0.1, -1.54, -1.7, 1.54, -0.2, 0]), np.zeros(7))),
+    'x0': np.concatenate((np.array([0., 0., 0., 0, 0., 0., 0]), np.zeros(7))),
     'dt': 0.05,
     'substeps': 5,
     'conditions': common['conditions'],
-    'pos_body_idx': np.array([1]),
-    'pos_body_offset': [[np.array([0.1, 0.1, 0])], [np.array([0.1, -0.1, 0])],
-                        [np.array([-0.1, -0.1, 0])], [np.array([-0.1, 0.1, 0])]],
+    'pos_body_idx': np.array([4]),
+    'pos_body_offset': [[np.array([0, 0, 0])]],
     'T': 100,
     'sensor_dims': SENSOR_DIMS,
     'state_include': [JOINT_ANGLES, JOINT_VELOCITIES, END_EFFECTOR_POINTS,
                       END_EFFECTOR_POINT_VELOCITIES],
     'obs_include': [JOINT_ANGLES, JOINT_VELOCITIES, END_EFFECTOR_POINTS,
                     END_EFFECTOR_POINT_VELOCITIES],
-    'camera_pos': np.array([0., 0., 2., 0., 0.2, 0.5]),
+    'camera_pos': np.array([2, -2, 3.5, 0, 0, 0]),
 }
 
 algorithm = {
     'type': AlgorithmBADMM,
     'conditions': common['conditions'],
     'iterations': 12,
-    'lg_step_schedule': np.array([1e-4, 1e-3, 1e-2, 1e-2]),
+    'lg_step_schedule': np.array([1e-2]), 
     'policy_dual_rate': 0.1,
     'init_pol_wt': 0.002,
-    'ent_reg_schedule': np.array([1e-3, 1e-3, 1e-2, 1e-1]),
+    'ent_reg_schedule': np.array([1e-2]),
     'fixed_lg_step': 3,
     'kl_step': 1.0,
-    'min_step_mult': 0.01,
-    'max_step_mult': 3.0,
+    'min_step_mult': 0.1,
+    'max_step_mult': 10.0,
     'sample_decrease_var': 0.05,
-    'sample_increase_var': 0.1,
+    'sample_increase_var': 0.5,
     'policy_sample_mode': 'replace'
 }
 
 algorithm['init_traj_distr'] = {
     'type': init_lqr,
-    'init_gains':  1.0 / PR2_GAINS,
+    'init_gains':  1.0 / GAINS,
     'init_acc': np.zeros(SENSOR_DIMS[ACTION]),
     'init_var': 1.0,
     'stiffness': 1.0,
@@ -105,35 +101,24 @@ algorithm['init_traj_distr'] = {
 
 torque_cost = {
     'type': CostAction,
-    'wu': 1e-3 / PR2_GAINS,
+    'wu': 1/GAINS * 1e-6,
 }
 
-fk_cost = {
+fk_cost = [{
     'type': CostFK,
-    'target_end_effector': np.array([0.0, 0.3, -0.5]),
-    'wp': np.array([2, 2, 1]),
+    'target_end_effector': np.array([0.2, -0.2, 0.25]),
+    'wp': np.array([1, 1, 1]),
     'l1': 0.1,
     'l2': 10.0,
-    'alpha': 1e-5,
-}
+    'alpha': 1e-2,
+    'ramp_option': RAMP_CONSTANT,
+} for i in xrange(common['conditions'])]
 
-# Create second cost function for last step only.
-final_cost = {
-    'type': CostFK,
-    'ramp_option': RAMP_FINAL_ONLY,
-    'target_end_effector': fk_cost['target_end_effector'],
-    'wp': fk_cost['wp'],
-    'l1': 1.0,
-    'l2': 0.0,
-    'alpha': 1e-5,
-    'wp_final_multiplier': 10.0,
-}
-
-algorithm['cost'] = {
+algorithm['cost'] = [{
     'type': CostSum,
-    'costs': [torque_cost, fk_cost, final_cost],
-    'weights': [1.0, 1.0, 1.0],
-}
+    'costs': [fk_cost[i]],
+    'weights': [6.0],
+} for i in xrange(common['conditions'])]
 
 algorithm['dynamics'] = {
     'type': DynamicsLRPrior,
